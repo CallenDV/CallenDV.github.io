bba558afd1ecbd7e1eb941843e896e68eca0bcac
CallenDV <callen12344@gmail.com>
2026-07-17 11:50:26 -0500

    slap me with a wet stinky fish because im STUPID

---
+++ new/gardenlink/gardenlink.js
@@ -0,0 +1,14 @@
+function gardenlinkthing() {
+    let sites = [
+        "https://thecheesespot.xyz/new/sandwich.html"
+    ]
+    let timer = 2;
+    let iframe = document.getElementById('iframe');
+
+    for(let i = 0; i < 10000000000; i++) {
+        let random = Math.floor(Math.random() * sites.length);
+        iframe.src = random;
+    }
+}
+
+gardenlinkthing(); 
